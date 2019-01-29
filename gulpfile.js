ee8ba2349143ad3aa953f8c71692e6897fb8c767
Martin Jarvis <freakent@gmail.com>
2019-01-29 13:13:43 +0000

    Updates to front page

--- gulpfile.js
+++ gulpfile.js
@@ -8,8 +8,18 @@ gulp.task('vendor', function() {
   gulp.src([
     "./node_modules/bootstrap/dist/js/bootstrap.min.*",
     "./node_modules/jquery/dist/jquery.slim.*",
-    "./node_modules/popper.js/dist/popper.min.*",
+    "./node_modules/popper.js/dist/umd/popper.min.*",
     "./node_modules/holderjs/holder.min.*",
   ])
     .pipe(gulp.dest("./assets/vendor"))
+
+  gulp.src([
+    "./node_modules/@fortawesome/fontawesome-free/scss/*",
+  ])
+    .pipe(gulp.dest("./_sass/fontawesome"))
+
+  gulp.src([
+    "./node_modules/@fortawesome/fontawesome-free/webfonts/*",
+  ])
+    .pipe(gulp.dest("./assets/vendor/fontawesome"))
 });
